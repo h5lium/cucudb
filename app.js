f48714848d68a5040a6d4408ae66f7caab9495f5
h5lium <h5lium@163.com>
2013-08-06 18:08:27 +0800

    mongo_init.js

--- app.js
+++ app.js
@@ -1,59 +1,75 @@
 // system dependencies
 var http = require('http'),
-    fs = require('fs');
-
-// non-system dependencies
+	fs = require('fs');
+//  public dependencies
 var express = require('express'),
-	_ = require('underscore');
+	_ = require('underscore'),
+	mongodb = require('mongodb');
+// private dependencies
+var mongo_init = require('./lib/mongo_init.js');
+
 
 // setup app
 var app = express();
-
-// global
-global = {
-	app: app,
-	appName: 'cucudb',
-	isOnBAE: fs.existsSync('./app/'),
-	_: _
-}
-
-// configure app
+// app config
 app.configure(function(){
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({
-    	secret: 'thats great'
-    }));
-    
-    // debug
-    /*app.use(function(req, res, next){
-    	console.log(req.body);
-    	next();
-    });*/
+	app.set('appName', 'cucudb');
+	app.set('isOnBAE', fs.existsSync('./app/'));
+	
+	if (! app.get('isOnBAE')) {
+		app.use(express.favicon());
+		app.use(express.logger('dev'));
+	}
+	
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret: 'thats great'
+	}));
+	
+	// debug
+	/*app.use(function(req, res, next){
+		console.log(req.body);
+		next();
+	});*/
 });
 
-// mongodb
-var mongodb = require('mongodb');
-new mongodb.MongoClient(new mongodb.Server('localhost', 27017, {
-	native_parser: true
-})).open(function(err, mongo) {
-	global.mongo = mongo;
+
+// db init done
+function dbInitDone(err, db){
+	app.set('db', db);
 	
 	 // router
-	require('./lib/router/');
+	require('./lib/router/')(app);
 	// static directory
-	app.use(express.static((global.isOnBAE? './app': '.') + '/static'));
+	app.use(express.static((app.get('isOnBAE')? './app': '.') + '/static'));
 	
 	// server
 	var server = http.createServer(app),
 		port = process.APP_PORT || process.env.PORT || 80;
 	
 	server.listen(port, function(){
-	    console.log(global.appName, 'listening on port ', port);
+		console.info(app.get('appName'), 'listening on port', port);
 	});
-});
+}
+
+
+// db config
+var dbConfig;
+if (app.get('isOnBAE')) {
+	dbConfig = {
+		dbName: 'jZLPkbYQyPBckRgYZtxK',
+		host: process.env.BAE_ENV_ADDR_MONGO_IP,
+		port: process.env.BAE_ENV_ADDR_MONGO_PORT,
+		username: process.env.BAE_ENV_AK,
+		password: process.env.BAE_ENV_SK
+	}
+} else {
+	dbConfig = {
+		dbName: app.get('appName')
+	}
+}
+// init mongo
+mongo_init(dbConfig, dbInitDone);
 
 
